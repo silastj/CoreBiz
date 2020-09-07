bd5007341253043c5924964916290730cbd84e42
aSilas <silastj@hotmail.com>
2020-09-06 23:36:49 -0300

    Correção na img do slide

--- projeto/src/components/partials/Slide/index.js
+++ projeto/src/components/partials/Slide/index.js
@@ -57,7 +57,7 @@ export default class SimpleSlider extends Component {
                         <h3>Olá, o que você está buscando?</h3>
                         <h2>Criar ou migrar seu e-commerce</h2>
                     </div>
-                    <img src="/assets/img/mobilef.png" alt="Corebiz"/>                    
+                    <img src="/assets/img/mobileF.png" alt="Corebiz"/>                    
                 </div>
 
 
